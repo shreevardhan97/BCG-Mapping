76c4997af387301a461ec0fbf253609e46a826a1
Shreevardhan Shah <shreevardhanshah@gmail.com>
2023-06-17 23:43:59 +0530

    added more csv

--- map2test.py
+++ map2test.py
@@ -67,6 +67,8 @@ for file in os.listdir('./Sangrur'):
         dfs.append(read_csv_and_concat('./Sangrur/' + file))
 
 df2temp = pd.concat(dfs).drop_duplicates()
+#state punjab
+df2temp = df2temp[df2temp['features__attributes__stname'] == 'PUNJAB']
 
 # df2temp = pd.concat([df2tempa, df2tempb]).drop_duplicates()
 #merge both df without 
@@ -139,6 +141,13 @@ def school_maps():
     m = folium.Map(location=[df2['LAT'].mean(), df2['LON'].mean()], zoom_start=10)
     #let the map cover the whole screen
     folium.TileLayer('cartodbpositron').add_to(m)
+    #highlight the selected district
+    folium.GeoJson(
+        df2,
+        name='geojson'
+    ).add_to(m)
+
+
 
     for i in range(0,len(df2)):
         mypopup = "School Name:{} \n Type:{} \n Management:{} \n Location:{} \n Pincode:{}".format(df2.iloc[i]['features__attributes__schname'], df2.iloc[i]['features__attributes__school_typ'], df2.iloc[i]['features__attributes__management'], df2.iloc[i]['features__attributes__sdtname'], df2.iloc[i]['features__attributes__pincode'])
